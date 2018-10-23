276db26712c692d5e73840a9120e85d2faeea2df
Chris Yocum <chryo12@yahoo.com>
2018-10-22 20:49:41 -0400

    Testing paused monitor label

--- status/script.js
+++ status/script.js
@@ -95,8 +95,8 @@ $(document).ready(function () {
 	}
 
 	function _uptimeRobotSetStatus(check) {
-		check.class = check.status === 0 ? 'label-paused';
-		check.text = check.status === 0 ? 'test paused';
+		//check.class = check.status === 0 ? 'label-paused';
+		//check.text = check.status === 0 ? 'test paused';
 		check.class = check.status === 2 ? 'label-success' : 'label-danger';
 		check.text = check.status === 2 ? 'Operational' : 'Major Outage';
 		if (check.status !== 2 && !check.lasterrortime) {
